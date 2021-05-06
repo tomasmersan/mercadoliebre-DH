9fb79b7f41d441c90737e1d798e2054685de5b88
tomasmersan <tomasmersanriera@MacBook-Pro-TMR.local>
2021-05-06 19:55:47 -0400

    cargué el archivo Procfile y modifiqué la ruta de acceso al servidor para Heroku

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const path = require ('path');
 
 const app = express();
 
+const process = require ('process');
+
 const publicPath = path.resolve(__dirname, './public');
 
 app.use (express.static(publicPath));
 
-app.listen(3000, () => 
+app.listen(process.env.PORT || 3000, () => 
 {
     console.log('Servidor corriendo en el puerto 3000');
 });
